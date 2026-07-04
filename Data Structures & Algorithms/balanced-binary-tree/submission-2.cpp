98134d33988fb56a983eb2b9583e01f0328c1dcd
Sophie Yen <112854696+sofiyen@users.noreply.github.com>
2026-07-04 22:51:12 +0800

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == nullptr) return true;
+        if(!isBalanced(root->left) || !isBalanced(root->right)) return false;
+        int left_h = getHeight(root->left);
+        int right_h = getHeight(root->right);
+        // debug
+        cout << left_h << " " << right_h << endl;
+        if(abs(left_h - right_h) > 1) return false;
+        return true;
+    }
+
+    int getHeight(TreeNode* cur){
+        if(cur == nullptr) return 0;
+
+        int left_h = getHeight(cur->left);
+        int right_h = getHeight(cur->right);
+
+        return 1 + max(left_h, right_h);
+    }
+};
